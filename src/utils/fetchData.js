ca6a561078a60918e975115cefcb5aa52feaf0f8
Harshitjain-22 <hjdec22@gmail.com>
2022-07-29 15:28:40 +0530

    generated exerciseDetails page and completed it partially: rendered its description till now , used an extra api called youtubeSearchAndDownloadVideos Api but still has to work on it

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -7,6 +7,14 @@ export const exerciseOptions = {
     }
   };
 
+  export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+      'X-RapidAPI-Key': "b4cc61b47emshdde0d5d4fe4567dp1203a2jsn8fac5d03baf1",
+    },
+  };
+
   export const fetchData = async (url, options) => {
     const res = await fetch(url, options);
     const data = await res.json();
